c1a8ae86638f2c6c028759c9457ad8c3af4b9c96
Mr Khaine <118953358+khainee@users.noreply.github.com>
2023-08-13 22:12:28 +0630

    Update sync.py

--- bot/modules/sync.py
+++ bot/modules/sync.py
@@ -50,8 +50,8 @@ async def start_sync(message, path, destination, listener):
     cmd = ["rclone", "sync", "--delete-during", "-P", f'--config={path}', f"{SOURCE}:", f"{destination}:"] 
     if config_dict["SERVER_SIDE"]:
         cmd.append("--server-side-across-configs")
-        cmd.append("--tpslimit=3")
-        cmd.append("--transfers=10")
+        #cmd.append("--tpslimit=3")
+        cmd.append("--transfers=20")
         cmd.append("--opendrive-chunk-size=32M")
 
     process = await exec(*cmd, stdout=PIPE, stderr=PIPE)
